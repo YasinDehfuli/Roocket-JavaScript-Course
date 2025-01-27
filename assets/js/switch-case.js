a963d1c4ced5f88f4fed93e7ac177312761d0077
YasinDehfuli <ydehfuli@gmail.com>
2025-01-27 14:51:00 +0330

    learn(switch-case): how to use switch case

---
+++ assets/js/switch-case.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const month = 8
+
+switch (month) {
+  case 1:
+    console.log('farvardin')
+    break
+  case 2:
+    console.log('ordibehesht')
+    break
+  case 3:
+    console.log('khordad')
+    break
+  case 4:
+    console.log('tir')
+    break
+  case 5:
+    console.log('mordad')
+    break
+  case 6:
+    console.log('shahrivar')
+    break
+  case 7:
+    console.log('mehr')
+    break
+  case 8:
+    console.log('aban')
+    break
+  case 9:
+    console.log('azar')
+    break
+  case 10:
+    console.log('dey')
+    break
+  case 11:
+    console.log('bahman')
+    break
+  case 12:
+    console.log('esfand')
+    break
+  
+  default:
+    console.log('Wrong!!!')
+    break
+}
+
+
+let num = 20;
+
+switch (true) {
+  case (num === 10 || num === 20):
+    console.log('it`s 10 or 20')
+    break
+  case (num > 30):
+    console.log('more than 30')
+    break
+  
+  default:
+    console.log('Wrong!!!')
+    break
+}
