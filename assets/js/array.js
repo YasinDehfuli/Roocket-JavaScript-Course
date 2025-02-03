63ae26e8f6362f758c8eee81e79dd63974624492
YasinDehfuli <ydehfuli@gmail.com>
2025-02-04 00:07:41 +0330

    learn(array): how to use arrays and loop on it

---
+++ assets/js/array.js
@@ -0,0 +1,42 @@
+let list = [20, 4, 6, 8, 12, 22, 99];
+
+// FOR
+// function sum(array) {
+//   let total = 0
+//
+//   for (let i =0; i < array.length; i++) {
+//   total += array[i]
+//     console.log('Total: ' + total + ' Item : ' + array[i])
+//   }
+// }
+
+// WHILE
+// function sum(array) {
+//   let i = 0
+//   let total = 0
+//
+//   while (i < array.length) {
+//     console.log('Total: ', total + ' Item : ' + array[i])
+//     total += array[i]
+//     i++
+//   }
+// }
+
+// FOREACH
+// function sum(array) {
+//   let total = 0
+//   array.forEach(item => {
+//     total += item;
+//     console.log(total);
+//   })
+// }
+
+// function sum(array) {
+//   array.reduce(function (total, item )  {
+//     total += item;
+//     console.log(total);
+//   })
+// }
+
+
+sum(list)
