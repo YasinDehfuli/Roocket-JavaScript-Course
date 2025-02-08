db9c291a8bbc36c082252d09ac1773d113a2e18b
YasinDehfuli <ydehfuli@gmail.com>
2025-02-08 14:38:02 +0330

    learn(objects): write object and add new details in it

--- assets/js/object.js
+++ assets/js/object.js
@@ -13,6 +13,14 @@ let car = {
   }
 }
 
+let car2 = new Object()
+car2.brand = 'benz'
+car2.model = 'e200'
+car2.color = 'white'
+car2.age = '2012'
+
 console.log('is this car sport?', car.details.sport)
 console.log('car details ==>', car)
 console.log('is this function runs? if runs log logger.', car.logger())
+
+console.log('car2 object details',car2)
