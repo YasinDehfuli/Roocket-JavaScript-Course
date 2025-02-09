4b3e9b3de7b08de1a8c0e11fd949d07069f7d754
YasinDehfuli <ydehfuli@gmail.com>
2025-02-09 12:47:23 +0330

    learn(object-extends): extends objects by prototype and object create in js

--- assets/js/objectConstructor.js
+++ assets/js/objectConstructor.js
@@ -5,11 +5,11 @@ function Car(brand, model, color, age) {
   this.age = age;
 }
 
-Object.prototype.parentProto = function(){
+Object.prototype.parentProto = function () {
   return 'Parent Proto';
 }
 
-Car.prototype.set = function (){
+Car.prototype.set = function () {
   return 'set'
 }
 
@@ -19,3 +19,23 @@ let car2 = new Car('benz', 'e220', 'white', '2016');
 
 console.log(car1);
 console.log(car2);
+
+let car3 = Object.create(car1);
+car3.brand = 'audi'
+car3.model = 'tt'
+car3.age = '2010'
+car3.makeSmile = function () {
+  console.log(':)')
+}
+console.log(car3);
+
+let car4 = Object.create(car3);
+car4.color = 'red'
+car4.test = 'case'
+car4.rings = 'sport'
+
+console.log(car4);
+
+let car5 = Object.create(car4);
+
+console.log(car5)
