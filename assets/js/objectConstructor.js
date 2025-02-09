10ae31918461f4557fa3ab49d44a3d3d5fed9104
YasinDehfuli <ydehfuli@gmail.com>
2025-02-09 12:24:19 +0330

    learn(objects-constructor): use object constructors prototype

--- assets/js/objectConstructor.js
+++ assets/js/objectConstructor.js
@@ -5,6 +5,14 @@ function Car(brand, model, color, age) {
   this.age = age;
 }
 
+Object.prototype.parentProto = function(){
+  return 'Parent Proto';
+}
+
+Car.prototype.set = function (){
+  return 'set'
+}
+
 let car1 = new Car('bmw', 'e46', 'red', '2006');
 
 let car2 = new Car('benz', 'e220', 'white', '2016');
