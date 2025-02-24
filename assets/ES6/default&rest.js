bc0ef5888b0a306f916e4c862b6df29345838311
YasinDehfuli <ydehfuli@gmail.com>
2025-02-24 14:15:53 +0330

    learn(function-arguments): learned to use arguments in functions for any arguments count

--- assets/ES6/default&rest.js
+++ assets/ES6/default&rest.js
@@ -1,8 +1,16 @@
 function sum(a = 1, b = 1) {
-  console.log('a:', a , 'b:', b)
+  console.log('a:', a, 'b:', b)
   console.log('a * b : ', a * b)
 }
 
 sum(2, 10)
 sum(10)
 
+
+function restArg(...args) {
+  return args.reduce((pre, current) => {
+    return pre + current
+  })
+}
+
+console.log(restArg(1, 2, 3, 4, 6, 100, 200, 300))
