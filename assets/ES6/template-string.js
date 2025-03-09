d3b36f8692bb5be32f6c9dd254304acbc7056095
YasinDehfuli <ydehfuli@gmail.com>
2025-03-09 14:41:50 +0330

    learn(template-string): learnder template string usage

---
+++ assets/ES6/template-string.js
@@ -0,0 +1,14 @@
+const number = 21
+const name = `your name is ${number}`
+
+
+console.log(name)
+
+const user = {
+  name: 'John',
+  family: 'Josefin Sans',
+}
+
+const userDetail = `user name: ${user.name} ${user.family}`
+
+console.log(userDetail)
