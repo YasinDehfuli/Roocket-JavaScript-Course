79bc2ca7632590625456ee875db1e70731df2cb6
YasinDehfuli <ydehfuli@gmail.com>
2025-03-21 19:48:44 +0330

    learn(ES7-8) learnde ES7-8 new methods include and pow

---
+++ assets/ES7-ES8/includes&pow.js
@@ -0,0 +1,9 @@
+let num1 = 2;
+let num2 = 4;
+
+console.log('pow numbers ==>',num1 ** num2);
+
+
+let array = ['vue', 'angular', 'react'];
+
+console.log('boolean search on array ==>', array.includes('vue'))
