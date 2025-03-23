107fcf7dbdbec73995f15466000ebede98206791
YasinDehfuli <ydehfuli@gmail.com>
2025-03-23 18:17:45 +0330

    project(video-player): added video player styles and scripts for play pause and forwarnd adn backward

---
+++ assets/projects/video-player/script.js
@@ -0,0 +1,42 @@
+const playerArea = document.querySelector('.myplayer');
+const video = playerArea.querySelector('video');
+const controls = playerArea.querySelector('.myplayer__controls')
+
+const play = controls.querySelector('.play')
+const rewind = controls.querySelector('.rewind')
+const forward = controls.querySelector('.forward')
+
+playerArea.addEventListener('click', () => {
+  if (video.paused) {
+    video.play()
+    doChangePlayerIcon()
+  } else {
+    video.pause()
+    doChangePlayerIcon()
+  }
+})
+
+play.addEventListener('click', () => {
+  if (video.paused) {
+    video.play()
+    doChangePlayerIcon()
+  } else {
+    video.pause()
+    doChangePlayerIcon()
+  }
+})
+
+rewind.addEventListener('click', () => {
+  video.currentTime -= 5
+})
+
+forward.addEventListener('click', () => {
+  video.currentTime += 5
+})
+
+function doChangePlayerIcon() {
+  const icon = playerArea.querySelector('.icon')
+  
+  icon.classList.toggle('ion-md-pause')
+  icon.classList.toggle('ion-md-play')
+}
