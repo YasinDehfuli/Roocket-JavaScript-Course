181b4d8c99995d98995239cef43c0be8e8e17f83
YasinDehfuli <ydehfuli@gmail.com>
2025-03-25 17:04:02 +0330

    project(typography): create moving shadows for item

--- assets/projects/typography/script.js
+++ assets/projects/typography/script.js
@@ -1,13 +1,24 @@
 const area = document.querySelector('.hero')
-const item = area.querySelector('.item')
-const limit = 500 // px
+const item = area.querySelector('h1')
+const limit = 140 // px
 
 area.addEventListener('mousemove', (mousePosition) => {
   const {offsetWidth: width, offsetHeight: height} = area
   
-   let x = mousePosition.x
-   let y = mousePosition.y
+  let x = mousePosition.x
+  let y = mousePosition.y
   
-  console.log( width, height, x, y )
+  let limitX = Math.round((x / width * limit))
+  let limitY = Math.round((y / width * limit))
+  
+  console.log(x, y, limitX, limitY)
+  
+  item.style.textShadow = `
+  ${limitX}px ${limitY}px 0 rgba(238,83,83,.7),
+  ${limitX * -1}px ${limitY}px 0 rgba(80,20,10,.7),
+  ${limitY}px ${limitX * -1}px 0 rgba(10,29,10,.7),
+  ${limitY * -1}px ${limitX }px 0 rgba(200,110,100,.7)
+  `
+  
   
 })
