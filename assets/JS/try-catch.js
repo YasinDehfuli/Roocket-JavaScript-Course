ef425e72dc6564bc2e161f4cb609d3c74c8c8325
YasinDehfuli <ydehfuli@gmail.com>
2025-04-13 14:38:17 +0330

    test commit

--- assets/JS/try-catch.js
+++ assets/JS/try-catch.js
@@ -2,7 +2,7 @@
 
 try{
   if (true){
-    throw {name: 'Wrong!', message: 'something wrong'};
+    throw {name: 'Wrong!!', message: 'something wrong'};
   }
 }catch (err){
   console.error(err.name , err.message);
